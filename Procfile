a81c117e721f9112d5b2dd64ef4ca80ce1b979f7
souravchakraborty700 <souravchakraborty700@gmail.com>
2024-08-07 05:16:30 +0530

    Fix asdi.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python run_daphne.py -p $PORT chattychat.asgi:application
+web: daphne -b 0.0.0.0 -p $PORT chattychat.asgi:application
--- chattychat/asgi.py
+++ chattychat/asgi.py
@@ -1,15 +1,13 @@
 import os
-import django
-from channels.routing import get_default_application
-from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing
+from channels.auth import AuthMiddlewareStack
+import chat.routing  # Adjust this import based on your actual app structure
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chattychat.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
-    "http": get_default_application(),
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
