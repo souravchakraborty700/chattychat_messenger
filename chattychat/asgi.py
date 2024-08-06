ed46e0044442ca63ce60265b9066729c9afafd5b
souravchakraborty700 <souravchakraborty700@gmail.com>
2024-08-07 03:50:45 +0530

    Initial commit 7

--- chattychat/asgi.py
+++ chattychat/asgi.py
@@ -4,7 +4,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', './settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chattychat.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- run_daphne.py
+++ run_daphne.py
@@ -8,3 +8,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chattychat.settings')  # Ensure
 django.setup()
 
 CommandLineInterface.entrypoint()
+
+# run_daphne.py
+# run_daphne.py
+# import os
+# import sys
+# from daphne.cli import CommandLineInterface
+
+# if __name__ == "__main__":
+#     port = os.environ.get('PORT', '8000')
+#     sys.argv = ["daphne", "-b", "0.0.0.0", "-p", port, "chattychat.asgi:application"]
+#     CommandLineInterface.entrypoint()
+
